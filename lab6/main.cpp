ba0da804c85cdd48002128b3688422dd21b894df
DeveloperMan313 <053000voloshch@gmail.com>
2024-03-22 02:09:11 +0300

    Fix polynomial addition and multiplication, make coefficient 0 terms be removed, fix stream input and output, add buffer overflow throw

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,9 +1,18 @@
 #include "Polynomial/Polynomial.hpp"
+#include <clocale>
 #include <iostream>
 
 int main() {
-  Polynomial p;
-  std::cin >> p;
-  std::cout << p << std::endl;
+  setlocale(LC_ALL, ".UTF8");
+  Polynomial p1, p2;
+  std::cout << "Введите полином p1" << std::endl;
+  std::cin >> p1;
+  std::cout << "Введите полином p2" << std::endl;
+  std::cin >> p2;
+  std::cout << std::endl;
+  std::cout << "p1 = " << p1 << std::endl;
+  std::cout << "p2 = " << p2 << std::endl;
+  std::cout << "p1 + p2 = " << p1 + p2 << std::endl;
+  std::cout << "p1 * p2 = " << p1 * p2 << std::endl;
   return 0;
 }
